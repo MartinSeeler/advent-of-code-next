109dd16b545867536563a80c7efa106bcfa67e92
Martin Seeler <developer@chasmo.de>
2022-11-30 17:46:01 +0100

    Add inital state to recoil root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
+import { queuedPuzzlePartsState } from "@/lib/atoms";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { FC } from "react";
 import { RecoilRoot } from "recoil";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <RecoilRoot>
+  <RecoilRoot
+    initializeState={({ set }) => {
+      set(queuedPuzzlePartsState, []);
+    }}
+  >
     <Component {...pageProps} />
   </RecoilRoot>
 );
