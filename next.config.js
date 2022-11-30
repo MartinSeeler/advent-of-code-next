d0e67eb8238c90f97da6cc6df16b1e0f079dbbba
Martin Seeler <developer@chasmo.de>
2022-11-30 19:54:03 +0100

    Add plausible tracking

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  rewrites() {
+    return [
+      {
+        source: "/stats/js/script.js",
+        destination: "https://plausible.io/js/script.outbound-links.js",
+      },
+      {
+        source: "/stats/api/event",
+        destination: "https://plausible.io/api/event",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,26 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { FC } from "react";
 import { RecoilRoot } from "recoil";
+import PlausibleProvider from "next-plausible";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <RecoilRoot
-    initializeState={({ set }) => {
-      set(queuedPuzzlePartsState, []);
+  <PlausibleProvider
+    domain="advent-of-code-next.vercel.app"
+    trackOutboundLinks={true}
+    scriptProps={{
+      src: "/stats/js/script.js",
+      // @ts-ignore
+      "data-api": "/stats/api/event",
     }}
   >
-    <Component {...pageProps} />
-  </RecoilRoot>
+    <RecoilRoot
+      initializeState={({ set }) => {
+        set(queuedPuzzlePartsState, []);
+      }}
+    >
+      <Component {...pageProps} />
+    </RecoilRoot>
+  </PlausibleProvider>
 );
 
 export default App;
