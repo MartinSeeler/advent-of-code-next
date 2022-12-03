54bcdce6837fd201597802293f5ae8c70d49d04a
Martin Seeler <developer@chasmo.de>
2022-12-03 15:50:10 +0100

    Add shorter ramda version for day03

--- puzzles/03/solution.ts
+++ puzzles/03/solution.ts
@@ -1,53 +1,111 @@
 import { Puzzle } from "@/lib/types";
 import {
-  isEmpty,
-  prepend,
-  take,
-  drop,
-  all,
-  includes,
-  uniq,
   sum,
-  slice,
   head,
-  curry,
-  compose,
   map,
+  splitEvery,
+  intersection,
+  split,
+  pipe,
+  trim,
+  prop,
+  __,
+  concat,
+  range,
+  zipObj,
+  reduce,
+  defaultTo,
 } from "ramda";
 import inputFile from "./input.txt";
 
-const parseLines = (input: string): string[] =>
-  input.split("\n").map((line) => line.trim());
+const parseLines = pipe(split("\n"), map(trim));
 
-const charScore = (char: string): number =>
-  char.charCodeAt(0) - (char.match(/[a-z]/) ? 96 : 38);
+const scores = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7,
+  h: 8,
+  i: 9,
+  j: 10,
+  k: 11,
+  l: 12,
+  m: 13,
+  n: 14,
+  o: 15,
+  p: 16,
+  q: 17,
+  r: 18,
+  s: 19,
+  t: 20,
+  u: 21,
+  v: 22,
+  w: 23,
+  x: 24,
+  y: 25,
+  z: 26,
+  A: 27,
+  B: 28,
+  C: 29,
+  D: 30,
+  E: 31,
+  F: 32,
+  G: 33,
+  H: 34,
+  I: 35,
+  J: 36,
+  K: 37,
+  L: 38,
+  M: 39,
+  N: 40,
+  O: 41,
+  P: 42,
+  Q: 43,
+  R: 44,
+  S: 45,
+  T: 46,
+  U: 47,
+  V: 48,
+  W: 49,
+  X: 50,
+  Y: 51,
+  Z: 52,
+};
 
-const window = <T>(n: number, list: T[]): T[][] =>
-  isEmpty(list) ? [] : prepend(take(n, list), window(n, drop(n, list)));
+const charScore = prop<number>(__, scores);
 
-const window3 = curry(window<string>)(3);
+const intersectAll = <T>(x: T[][]): T[] =>
+  x.length === 1 ? x[0] : intersection(x[0], intersectAll(x.slice(1)));
 
-const lineToHalfs = (line: string): string[] => [
-  slice(0, line.length / 2, line),
-  slice(line.length / 2, line.length, line),
-];
+const chunkToScore: (chunks: string[]) => number = pipe(
+  map<string, string[]>(split("")),
+  intersectAll,
+  (xs: string[]) => defaultTo("", head(xs)),
+  charScore
+);
 
-const isInAll = (chunks: string[]) => (x: string) => all(includes(x), chunks);
-
-const chunkToScore = (chunks: string[]) =>
-  head(uniq(chunks[0].split("")).filter(isInAll(chunks)).map(charScore)) || 0;
+const part1 = pipe(
+  parseLines,
+  map((x) => splitEvery(x.length / 2, x)),
+  map(chunkToScore),
+  sum
+);
+const part2 = pipe(parseLines, splitEvery(3), map(chunkToScore), sum);
 
 async function solvePart1(input: string): Promise<number> {
-  return compose(sum, map(chunkToScore), map(lineToHalfs), parseLines)(input);
+  return part1(input);
 }
 
 async function solvePart2(input: string): Promise<number> {
-  return compose(sum, map(chunkToScore), window3, parseLines)(input);
+  return part2(input);
 }
 
 export default {
   day: "03",
-  name: "Rucksack Reorganization",
+  name: "cksack organization",
   input: inputFile,
   solvePart1,
   solvePart2,
