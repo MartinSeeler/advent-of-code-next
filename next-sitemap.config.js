88bb1a2045b7cade7260fd481402d2a1ac414d5d
Martin Seeler <developer@chasmo.de>
2022-12-06 08:57:04 +0100

    Add sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://advent-of-code-next.vercel.app",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+};
