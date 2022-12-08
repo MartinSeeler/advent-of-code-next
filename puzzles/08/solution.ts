c67f546b4400347e49903dd48251554b0c7b6683
Martin Seeler <developer@chasmo.de>
2022-12-08 10:54:05 +0100

    Add faster solution for day08 part 1

--- puzzles/08/solution.ts
+++ puzzles/08/solution.ts
@@ -1,5 +1,22 @@
 import { Puzzle } from "@/lib/types";
-import { all, flip, lt, reverse, takeWhile, transpose, uniq } from "ramda";
+import {
+  all,
+  contains,
+  equals,
+  filter,
+  flip,
+  identity,
+  includes,
+  length,
+  lt,
+  map,
+  pipe,
+  reverse,
+  sum,
+  takeWhile,
+  transpose,
+  uniq,
+} from "ramda";
 import inputFile from "./input.txt";
 
 type Grid = number[][];
@@ -18,17 +35,13 @@ const parseGrid = (input: string): Grid =>
 const canBeIgnored = (x: number, y: number, w: number, h: number) =>
   x === 0 || x === w || y === 0 || y === h;
 
-const filterFreeIndices = (grid: Grid, transposed: boolean = false) =>
-  grid.flatMap((row: number[], y: number) =>
+const filterFreeIndices = (grid: Grid): boolean[][] =>
+  grid.map((row: number[], y: number) =>
     row.map((height: number, x: number) =>
       canBeIgnored(x, y, row.length - 1, grid.length - 1)
-        ? []
+        ? false
         : all(flip(lt)(height), row.slice(0, x)) ||
           all(flip(lt)(height), row.slice(x + 1))
-        ? transposed
-          ? [y, x]
-          : [x, y]
-        : []
     )
   );
 
@@ -46,10 +59,24 @@ async function solvePart1(input: string): Promise<number> {
   const grid = parseGrid(input);
   const borderLength = grid.length * 2 + (grid[0].length - 2) * 2;
 
-  const freeIndicesHor = filterFreeIndices(grid, false);
-  const freeIndicesVer = filterFreeIndices(transpose(grid), true);
-  const uniqueIndices = uniq([...freeIndicesHor, ...freeIndicesVer]);
-  return borderLength + uniqueIndices.length - 1;
+  const freeIndicesHor = filterFreeIndices(grid);
+  const freeIndicesVer = transpose(filterFreeIndices(transpose(grid)));
+  const result = freeIndicesHor.map((row: boolean[], x: number) =>
+    row.map(
+      (visible: boolean, index: number) => visible || freeIndicesVer[x][index]
+    )
+  );
+
+  // ramda version to count all true values in matrix
+  const count = pipe(
+    // @ts-ignore
+    map(pipe(filter<boolean, boolean>(identity), length)),
+    sum
+    // @ts-ignore
+  )(result);
+  console.log(count);
+
+  return borderLength + count;
 }
 
 async function solvePart2(input: string): Promise<number> {
