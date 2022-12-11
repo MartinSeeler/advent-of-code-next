cba138cdbc9351ab843eccd64564295419cafd66
Martin Seeler <developer@chasmo.de>
2022-12-11 08:57:27 +0100

    Add solution for day11

---
+++ puzzles/11/solution.test.ts
@@ -0,0 +1,77 @@
+import puzzle from "./solution";
+
+describe("Day 11 - Monkey in the Middle", () => {
+  it.each([
+    [
+      `Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+`,
+      10605,
+    ],
+  ])("should solve part 1", async (sampleInput: string, expected: number) => {
+    const result = await puzzle.solvePart1(sampleInput);
+    expect(result).toEqual(expected);
+  });
+
+  it.each([
+    [
+      `Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+`,
+      2713310158,
+    ],
+  ])("should solve part 2", async (sampleInput: string, expected: number) => {
+    const result = await puzzle.solvePart2(sampleInput);
+    expect(result).toEqual(expected);
+  });
+});
---
+++ puzzles/11/solution.ts
@@ -0,0 +1,136 @@
+import { Puzzle } from "@/lib/types";
+import {
+  add,
+  append,
+  divide,
+  head,
+  length,
+  lensIndex,
+  lensProp,
+  match,
+  modulo,
+  multiply,
+  over,
+  pluck,
+  range,
+  reduce,
+  sort,
+  view,
+} from "ramda";
+import { floor } from "ramda-adjunct";
+import inputFile from "./input.txt";
+
+import lcm from "lcm";
+
+type Item = number;
+
+type Monkey = {
+  items: Item[];
+  op: (x: Item) => Item;
+  test: number;
+  trueMonkey: number;
+  falseMonkey: number;
+  inspections: number;
+};
+
+type Transfer = [Item, number];
+
+const square = (x: number) => x * x;
+const double = (x: number) => x + x;
+
+const parseMonkeys = (input: string): Monkey[] =>
+  input
+    .trim()
+    .split("\n\n")
+    .map((block) => {
+      const lines = block.split("\n").map((x) => x.trim());
+      const items = match(/\d+/g, lines[1]).map((x) => parseInt(x, 10));
+
+      const opNum = head(match(/\d+/g, lines[2]).map((x) => parseInt(x, 10)));
+      const isMult = lines[2].includes("*");
+      const op = isMult
+        ? opNum
+          ? multiply(opNum)
+          : square
+        : opNum
+        ? add(opNum)
+        : double;
+
+      const test = match(/\d+/g, lines[3]).map((x) => parseInt(x, 10))[0];
+      const trueMonkey = match(/\d+/g, lines[4]).map((x) => parseInt(x, 10))[0];
+      const falseMonkey = match(/\d+/g, lines[5]).map((x) =>
+        parseInt(x, 10)
+      )[0];
+
+      return {
+        items,
+        op,
+        test,
+        trueMonkey,
+        falseMonkey,
+        inspections: 0,
+      };
+    });
+
+const runMonkey = (monkey: Monkey, lcm: number): Transfer[] =>
+  monkey.items.reduce((acc, item) => {
+    const result = monkey.op(item);
+    const result2 = lcm ? modulo(result, lcm) : floor(divide(result, 3));
+    return append(
+      [
+        result2,
+        modulo(result2, monkey.test) ? monkey.falseMonkey : monkey.trueMonkey,
+      ] as Transfer,
+      acc
+    );
+  }, [] as Transfer[]);
+
+const resetMonkey =
+  (inspections: number) =>
+  (monkey: Monkey): Monkey => ({
+    ...monkey,
+    inspections: inspections + monkey.inspections,
+    items: [],
+  });
+
+const runMonkeys = (monkeys: Monkey[], lcm: number): Monkey[] =>
+  monkeys.reduce((acc, _, idx) => {
+    const transfers = runMonkey(view(lensIndex(idx), acc), lcm);
+    const appended = transfers.reduce(
+      (acc2, [item, monkeyIdx]) =>
+        over(lensIndex(monkeyIdx), over(lensProp("items"), append(item)), acc2),
+      acc
+    );
+    return over(lensIndex(idx), resetMonkey(length(transfers)), appended);
+  }, monkeys);
+
+const simulate = (
+  monkeys: Monkey[],
+  rounds: number,
+  lcm: number = 0
+): Monkey[] =>
+  reduce((acc, _) => runMonkeys(acc, lcm), monkeys, range(0, rounds));
+
+const monkeyBusiness = (monkeys: Monkey[]): number => {
+  const top: number[] = sort((l, r) => r - l, pluck("inspections", monkeys));
+  return multiply(top[0], top[1]);
+};
+
+async function solvePart1(input: string): Promise<number> {
+  return monkeyBusiness(simulate(parseMonkeys(input), 20));
+}
+
+async function solvePart2(input: string): Promise<number> {
+  const monkeys = parseMonkeys(input);
+  return monkeyBusiness(
+    simulate(monkeys, 10000, pluck("test", monkeys).reduceRight(lcm, 1))
+  );
+}
+
+export default {
+  day: "11",
+  name: "Monkey in the Middle",
+  input: inputFile,
+  solvePart1,
+  solvePart2,
+} as Puzzle;
